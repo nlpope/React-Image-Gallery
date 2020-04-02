94e7d8b0d945b0fa59ecaefc7c020e4300edc894
Noah Pope <nlpope@tutanota.com>
2020-04-02 12:32:30 -0700

    deleting index.html comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
 
-//functional components use return()
 class App extends React.Component {
   state = { images: [] };
   onSearchSubmit = async term => {
